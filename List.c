914d53c062882325ea21279ca8808dc5b87ec1d7
StathisKap <stathiskap75@gmail.com>
2021-05-07 19:39:52 +0100

    Included List.c

--- List.c
+++ List.c
@@ -6,19 +6,16 @@
 
 char * List(FILE *output, char * word)
 {
-    char * word2 = (char*)malloc(20);
+    char * word2 = (char*)calloc(20,1);
     size_t i;
     for (i = 0; i < strlen(word); i++)
     {
-        if (isalpha(word[i]))
+        if (isalpha(word[i]) || word[i] == '-' || word[i] == '\'')
         {
             word2[i] = tolower(word[i]);
         }
-        else
-        word++;
     }
         word2[i] = '\0'; 
     
-    // fprintf(output,"%s\n",word2);
     return word2;
 }
